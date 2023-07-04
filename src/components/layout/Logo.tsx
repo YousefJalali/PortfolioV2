7e4fec40a6ca08a610ad24464672c7f82057f4e2
Jbygoal <jbtgoal@gmail.com>
2023-07-04 19:18:34 +0300

    update showcases

--- src/components/layout/Logo.tsx
+++ src/components/layout/Logo.tsx
@@ -1,28 +1,30 @@
 export default function Logo() {
   return (
-    <a className='h-8 w-8 lg:h-12 lg:w-12'>
-      <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 652.7 677.27'>
-        <polygon
-          points='650.9 399.75 650.9 677.27 527.66 677.27 527.66 429.81 248.14 0 395.42 0 650.9 399.75'
-          className='fill-neutral'
-        />
-        <polygon
-          points='466.91 454.89 466.91 677.27 343.68 677.27 343.68 484.95 29.37 0 176.65 0 466.91 454.89'
-          className='fill-neutral'
-        />
-        <polygon
-          points='114.13 549.43 377 549.43 377 677.01 29.37 677.01 114.13 549.43'
-          className='fill-neutral'
-        />
-        <polygon
-          points='594.19 56.06 595.61 228.59 652.7 317.92 652.7 0 449.52 0 484.7 55.05 594.19 56.06'
-          className='fill-base-300'
-        />
-        <polygon
-          points='0 24.05 0 647.75 57.09 558.42 58.34 114.07 0 24.05'
-          className='fill-base-300'
-        />
-      </svg>
-    </a>
+    <svg
+      xmlns='http://www.w3.org/2000/svg'
+      viewBox='0 0 652.7 677.27'
+      className='h-8 w-8 lg:h-12 lg:w-12'
+    >
+      <polygon
+        points='650.9 399.75 650.9 677.27 527.66 677.27 527.66 429.81 248.14 0 395.42 0 650.9 399.75'
+        className='fill-neutral'
+      />
+      <polygon
+        points='466.91 454.89 466.91 677.27 343.68 677.27 343.68 484.95 29.37 0 176.65 0 466.91 454.89'
+        className='fill-neutral'
+      />
+      <polygon
+        points='114.13 549.43 377 549.43 377 677.01 29.37 677.01 114.13 549.43'
+        className='fill-neutral'
+      />
+      <polygon
+        points='594.19 56.06 595.61 228.59 652.7 317.92 652.7 0 449.52 0 484.7 55.05 594.19 56.06'
+        className='fill-base-300'
+      />
+      <polygon
+        points='0 24.05 0 647.75 57.09 558.42 58.34 114.07 0 24.05'
+        className='fill-base-300'
+      />
+    </svg>
   )
 }
